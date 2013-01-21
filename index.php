c748fd11187144eef98439eeb882d8a54ecca518
Ali <aliasgar@outlook.com>
2013-01-21 22:44:09 +0530

    Added Some Data

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
  <?php
  	
- 	include('config.ini');
+ 	include_once('config.ini');
  	$query = "SELECT employee_id, last_name, first_name, title "
  		."FROM employees ORDER BY last_name ASC, first_name ASC";
  	$result = pg_query($db, $query);
- 	print_r($result);
- 	echo $result;
- 	echo "Done";
+ 	$final = array();
+ 	while($row = pg_fetch_assoc($result)) {
+ 		$final[] = $row;
+ 	}
+ 	echo json_encode($final);
  	
 ?>
 <?php
