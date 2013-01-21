1a138d428aade85f95a80d8b2c6f497533678452
Ali <aliasgar@outlook.com>
2013-01-21 22:26:12 +0530

    Added Some Data

--- first_run.php
+++ first_run.php
@@ -1,3 +1,5 @@
 <?php
 	include_once('config.ini');
+	pg_query($db,"create table account");
+	
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
  <?php
  	include('config.ini');
- 	echo "hello world";
+ 	$query = "SELECT employee_id, last_name, first_name, title "
+ 		."FROM employees ORDER BY last_name ASC, first_name ASC";
+ 	$result = pg_query($db, $query);
+ 	print_r($result);
 ?>
