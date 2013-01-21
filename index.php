52931275d12c0f8be7c88ebd9aebfa3303e5c6fa
Ali <aliasgar@outlook.com>
2013-01-21 22:47:06 +0530

    Added Some Data

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
- <?php
- 	
- 	include_once('config.ini');
- 	$query = "SELECT employee_id, last_name, first_name, title "
- 		."FROM employees ORDER BY last_name ASC, first_name ASC";
- 	$result = pg_query($db, $query);
- 	$final = array();
- 	while($row = pg_fetch_assoc($result)) {
- 		$final[] = $row;
- 	}
- 	echo json_encode($final);
- 	
-?>
-<?php
-	include_once("connection.ini");
-?>
-<html>
+ <html>
  <head>
   <title>Employees</title>
  </head>
@@ -29,20 +13,25 @@
     </tr>
    </thead>
    <tbody>
-<?php
-$query = "SELECT employee_id, last_name, first_name, title "
-    . "FROM employees ORDER BY last_name ASC, first_name ASC";
-$result = $db->query($query);
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<tr>";
-    echo "<td>" . $row["employee_id"] . "</td>";
-    echo "<td>" . htmlspecialchars($row["last_name"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
-    echo "<td>" . htmlspecialchars($row["title"]) . "</td>";
-    echo "</tr>";
-}
-$result->closeCursor();
+ <?php
+ 	
+ 	include_once('config.ini');
+ 	$query = "SELECT employee_id, last_name, first_name, title "
+ 		."FROM employees ORDER BY last_name ASC, first_name ASC";
+ 	$result = pg_query($db, $query);
+
+ 	while($row = pg_fetch_assoc($result)) {
+ 		echo "<tr>";
+	    echo "<td>" . $row["employee_id"] . "</td>";
+	    echo "<td>" . htmlspecialchars($row["last_name"]) . "</td>";
+	    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
+	    echo "<td>" . htmlspecialchars($row["title"]) . "</td>";
+	    echo "</tr>";
+ 	}
+
+ 	
 ?>
+
    </tbody>
   </table>
  </body>
