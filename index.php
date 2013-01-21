3d4c29718263cdd56ba6f14063d34cb7df5df696
Ali <aliasgar@outlook.com>
2013-01-21 22:41:29 +0530

    Added Some Data

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <?php
  	
- 	include('config.ini.php');
+ 	include('config.ini');
  	$query = "SELECT employee_id, last_name, first_name, title "
  		."FROM employees ORDER BY last_name ASC, first_name ASC";
  	$result = pg_query($db, $query);
