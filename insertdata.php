68001d3a56b4e8a8bb9c4d2e4a9faa50b9f7cb00
Ali <aliasgar@outlook.com>
2013-01-22 20:29:07 +0530

    Trying again.. hopefully works..

--- insertdata.php
+++ insertdata.php
@@ -15,12 +15,12 @@
 		}
 		$i++;
 	}
-	echo implode(",",$field_name_row)."<br/>";
-	echo implode(",",$field_value_row)."<br/>";
+	// echo implode(",",$field_name_row)."<br/>";
+	// echo implode(",",$field_value_row)."<br/>";
 	// $field_name_row = array("last_name","first_name","title");
 	// $field_value_row = array($_POST["last_name"],$_POST["first_name"],$_POST["title"]);
 
-	$query = "INSERT INTO employees ("`.implode("`, `",$field_name_row).`") VALUES ('".implode("', '",$field_value_row)."');";
+	$query = "INSERT INTO employees (`".implode("`, `",$field_name_row)."`) VALUES ('".implode("', '",$field_value_row)."');";
 	echo $query ."<br/>";
 	//pg_query = ($db, $query);
 	//echo $_POST["last_name"]."<br/>".$_POST["first_name"]."<br/>".$_POST["title"];
