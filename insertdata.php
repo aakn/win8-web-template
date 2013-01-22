73ae7ee168df5e6593e9d0f7df0884b70ca7efc4
Ali <aliasgar@outlook.com>
2013-01-22 20:16:00 +0530

    Might have fixed the problem

--- insertdata.php
+++ insertdata.php
@@ -2,25 +2,25 @@
 	include_once('config.ini');
 	$result = pg_query($db, "select * from employees limit 1");
 	$i = 1;
-	// $field_name_row = array();
-	// $field_value_row = array();
-	// while($i < pg_num_fields($result)) 
-	// {
-	// 	$fieldName = pg_field_name($result, $i);
+	$field_name_row = array();
+	$field_value_row = array();
+	while($i < pg_num_fields($result)) 
+	{
+		$fieldName = pg_field_name($result, $i);
 		
-	// 	$value = $_POST[$fieldName];
-	// 	if($value) {
-	// 		$field_name_row[] = $fieldName;
-	// 		$field_value_row[] = $value;
-	// 	}
-	// 	$i++;
-	// }
+		$value = $_POST[$fieldName];
+		if($value) {
+			$field_name_row[] = $fieldName;
+			$field_value_row[] = $value;
+		}
+		$i++;
+	}
 
-	// $field_name_row = array("last_name","first_name","title");
-	// $field_value_row = array($_POST["last_name"],$_POST["first_name"],$_POST["title"]);
+	$field_name_row = array("last_name","first_name","title");
+	$field_value_row = array($_POST["last_name"],$_POST["first_name"],$_POST["title"]);
 
-	// $query = "insert into employees (".implode(",",$field_name_row).") values (".implode(",",$field_value_row).")";
-	// pg_query = ($db, $query);
-	echo $_POST["last_name"]."<br/>".$_POST["first_name"]."<br/>".$_POST["title"];
+	$query = "insert into employees (".implode(",",$field_name_row).") values (".implode(",",$field_value_row).")";
+	pg_query = ($db, $query);
+	//echo $_POST["last_name"]."<br/>".$_POST["first_name"]."<br/>".$_POST["title"];
 	echo "inserted successfully";
 ?>
