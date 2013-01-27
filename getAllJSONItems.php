ce31247361a80f681c1739db3f6b9f162c3dff21
Ali <aliasgar@outlook.com>
2013-01-27 14:48:30 +0530

    Added get JSON Items file

---
+++ getAllJSONItems.php
@@ -0,0 +1,11 @@
+<?php
+	include_once('config.ini');
+	$query = "SELECT * from template";
+
+	$result = pg_query($db, $query);
+	$rows=array();
+	while($row = pg_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+	echo json_encode($rows);
+?>
