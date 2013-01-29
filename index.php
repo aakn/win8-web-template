4fe2cedfcf987233557450a0d7d011b8a1cb9ab1
Ali <aliasgar@outlook.com>
2013-01-29 22:12:18 +0530

    added modals

--- index.php
+++ index.php
@@ -113,8 +113,7 @@
 										?>
 										
 										<td>
-											<button class="btn btn-inverse edit-btn" <?php echo "id='$id'" ?> >Edit</button>
-											<button class="btn btn-danger delete-btn" <?php echo "id='$id'" ?> >Delete</button>
+											<button class="btn btn-danger delete-btn" data-toggle="modal" href="#edit-modal" <?php echo "id='$id'" ?> >Delete</button>
 										</td>
 										<?php
 										echo "</tr>";
