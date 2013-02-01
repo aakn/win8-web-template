d97b123b38225eb087c69df3956842b4bba034a5
Ali <aliasgar@outlook.com>
2013-02-01 12:17:28 +0530

    added populate table file

--- fillTable.php
+++ fillTable.php
@@ -1,7 +1,7 @@
 <?php								
 	include_once('config.ini');
 	
-	$i=1;
+	$i=2;
 	$j=0;
 	$item3="This is a sample item";
 	$item4="This is a sample sample sample sample sample item";
@@ -18,6 +18,27 @@
 		$i++;
 	//}
 	
+	$item1 = "group$i";
+		while($j<10){
+			$item2="subitem$j";
+
+			$query = "INSERT INTO template (item1, item2,item3,item4) VALUES ('$item1','$item2','$item3','$item4');";
+			$result = pg_query($db, $query);
+			echo "$i $j $query<br/>";
+			$j++;
+		}
+		$i++;
+
+		$item1 = "group$i";
+		while($j<10){
+			$item2="subitem$j";
+
+			$query = "INSERT INTO template (item1, item2,item3,item4) VALUES ('$item1','$item2','$item3','$item4');";
+			$result = pg_query($db, $query);
+			echo "$i $j $query<br/>";
+			$j++;
+		}
+		$i++;
 
 	echo "Inserted Successfully<br/>";
 
