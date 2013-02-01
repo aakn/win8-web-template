bb4aae25827b173fea64a25541ab600cd731c4b8
Ali <aliasgar@outlook.com>
2013-02-01 12:19:41 +0530

    added populate table file

--- fillTable.php
+++ fillTable.php
@@ -1,44 +1,24 @@
 <?php								
 	include_once('config.ini');
 	
-	$i=2;
-	$j=0;
+	$i=3;
+	
 	$item3="This is a sample item";
 	$item4="This is a sample sample sample sample sample item";
-	//while($i<10) {
+	while($i<10) {
+		$j=0;
 		$item1 = "group$i";
 		while($j<10){
 			$item2="subitem$j";
 
 			$query = "INSERT INTO template (item1, item2,item3,item4) VALUES ('$item1','$item2','$item3','$item4');";
 			$result = pg_query($db, $query);
-			echo "$i $j $query<br/>";
+			echo "$query<br/>";
 			$j++;
 		}
 		$i++;
-	//}
+	}
 	
-	$item1 = "group$i";
-		while($j<10){
-			$item2="subitem$j";
-
-			$query = "INSERT INTO template (item1, item2,item3,item4) VALUES ('$item1','$item2','$item3','$item4');";
-			$result = pg_query($db, $query);
-			echo "$i $j $query<br/>";
-			$j++;
-		}
-		$i++;
-
-		$item1 = "group$i";
-		while($j<10){
-			$item2="subitem$j";
-
-			$query = "INSERT INTO template (item1, item2,item3,item4) VALUES ('$item1','$item2','$item3','$item4');";
-			$result = pg_query($db, $query);
-			echo "$i $j $query<br/>";
-			$j++;
-		}
-		$i++;
 
 	echo "Inserted Successfully<br/>";
 
