d323a6ff45598f5cdc0027a6623ad0de65d99cc9
Ali <aliasgar@outlook.com>
2013-02-11 21:34:44 +0530

    updated upload code

--- php/show-images.php
+++ php/show-images.php
@@ -1,11 +1,11 @@
 <?php
-	include_once('config.ini');
+	include_once('../config.ini');
 	$query = "SELECT * from photos";
 	$result = pg_query($db, $query);
 	$num = pg_num_rows($result);
 	echo "number of rows: $num <br/>";
 	$rows=array();
-	while($row = pg_fetch_assoc($result)) {
+	while($row = pg_fetch_row($result)) {
 		$rows[] = $row;
 		//$image = $row.photo;
 		//echo "<img src='$image'/><br/>";
