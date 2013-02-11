eecd7254f8163fb2ba6cfdf9ee1058f9424f79bf
Ali <aliasgar@outlook.com>
2013-02-11 21:50:17 +0530

    updated upload code

--- php/show-images.php
+++ php/show-images.php
@@ -1,17 +1,19 @@
 <?php
 	include_once('../config.ini');
-	$query = "SELECT * from photos";
+	$query = "SELECT * from photos LIMIT 2";
 	$result = pg_query($db, $query);
 	$num = pg_num_rows($result);
 	echo "number of rows: $num <br/>";
 	$rows=array();
-	while($row = pg_fetch_row($result)) {
-		$rows[] = $row;
+	while($row = pg_fetch_assoc($result)) {
+		$row.photo = stripslashes($row.photo);
+
 		$image = $row.photo;
 		var_dump($image);
 		echo $image."<br/>";
 		echo "img src='$image'<br/>";
 		echo "<img src='$image'/><br/>";
+		$rows[] = $row;
 	}
 	echo json_encode($rows);
 ?>
