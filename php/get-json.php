29284a36448c73715d5c7fe72401c070440fe59b
Ali <aliasgar@outlook.com>
2013-02-20 15:50:35 +0530

    added images

--- php/get-json.php
+++ php/get-json.php
@@ -15,10 +15,6 @@
 	else if ($page="category") {
 		$rows = array_values(array_filter($rows,"subcategory"));
 
-		if(($key = array_search("Fit and Flare", $rows)) !== false) {
-		    unset($messages[$key]);
-		}
-		array_unshift($rows, "Fit and Flare");
 	}
 
 	
